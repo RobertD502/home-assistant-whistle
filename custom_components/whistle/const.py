93f49e30bbfbe1e3d01ba96c70115e842c169973
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2022-08-30 14:58:30 -0400

    rewrite

--- custom_components/whistle/const.py
+++ custom_components/whistle/const.py
@@ -1,3 +1,28 @@
-"""Whistle platform constants"""
+""" Constants for Whistle """
 
+import asyncio
+import logging
+
+from aiohttp.client_exceptions import ClientConnectionError
+
+from homeassistant.const import Platform
+
+from whistleaio.exceptions import WhistleAuthError
+
+LOGGER = logging.getLogger(__package__)
+
+DEFAULT_SCAN_INTERVAL = 60
 DOMAIN = "whistle"
+PLATFORMS = [
+    Platform.DEVICE_TRACKER,
+    Platform.SENSOR,
+]
+
+DEFAULT_NAME = "Whistle"
+TIMEOUT = 20
+
+WHISTLE_ERRORS = (
+    asyncio.TimeoutError,
+    ClientConnectionError,
+    WhistleAuthError,
+)
