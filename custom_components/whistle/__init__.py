5241a78f7698f477b5e95fd1442e89965e9f707b
RobertD502 <52541649+RobertD502@users.noreply.github.com>
2022-09-13 16:41:01 -0400

    migrate to version 3, add update listener

--- custom_components/whistle/__init__.py
+++ custom_components/whistle/__init__.py
@@ -1,15 +1,20 @@
 """ Whistle Component """
 from __future__ import annotations
 
-from whistleaio.exceptions import WhistleAuthError
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN, LOGGER, PLATFORMS
+from .const import (
+    CONF_ZONE_METHOD,
+    DEFAULT_ZONE_METHOD,
+    DOMAIN,
+    LOGGER,
+    PLATFORMS,
+    UPDATE_LISTENER,
+    WHISTLE_COORDINATOR,
+)
 from .coordinator import WhistleDataUpdateCoordinator
-from .util import async_validate_api, NoPetsError
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -17,9 +22,14 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     coordinator = WhistleDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
+        WHISTLE_COORDINATOR: coordinator
+    }
+    
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    update_listener = entry.add_update_listener(async_update_options)
+    hass.data[DOMAIN][entry.entry_id][UPDATE_LISTENER] = update_listener
 
     return True
 
@@ -28,6 +38,8 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """ Unload Whistle config entry. """
 
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        update_listener = hass.data[DOMAIN][entry.entry_id][UPDATE_LISTENER]
+        update_listener()
         del hass.data[DOMAIN][entry.entry_id]
         if not hass.data[DOMAIN]:
             del hass.data[DOMAIN]
@@ -37,26 +49,28 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """ Migrate old entry. """
 
-    if entry.version == 1:
-        email = entry.data[CONF_USERNAME]
+    if entry.version in [1,2]:
+        if entry.version == 1:
+            email = entry.data[CONF_USERNAME]
+        else:
+            email = entry.data[CONF_EMAIL]
         password = entry.data[CONF_PASSWORD]
 
-        try:
-            await async_validate_api(hass, email, password)
-        except (WhistleAuthError, ConnectionError, NoPetsError):
-            return False
-
-        entry.version = 2
-
         LOGGER.debug(f'Migrate Whistle config entry unique id to {email}')
+        entry.version = 3
+
         hass.config_entries.async_update_entry(
             entry,
             data={
                 CONF_EMAIL: email,
                 CONF_PASSWORD: password,
-
             },
+            options={CONF_ZONE_METHOD: DEFAULT_ZONE_METHOD},
             unique_id=email,
         )
-
     return True
+
+async def async_update_options(hass: HomeAssistant, entry: ConfigEntry) -> None:
+    """ Update options. """
+    
+    await hass.config_entries.async_reload(entry.entry_id)
